b7e6f869393064d670bbf29cd2e54b70d7400367
sanyaazar <sacha12092003@gmail.com>
2023-03-19 14:05:34 +0300

    this is the first

--- mll.java
+++ mll.java
@@ -1,5 +1,8 @@
 public class mll {
     public static void main(String[] args) {
-        System.out.println("nulllll");
+        System.out.println("it's real");
+        int a = 10;
+        double oao = 10.0;
+        float o = 10;
     }
 }
